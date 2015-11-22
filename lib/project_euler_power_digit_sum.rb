0edeec7c7442010506311e1520191e01bfd79063
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2015-11-22 22:32:13 +0000

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,16 @@
 # code your solution here
+def power_digit_sum(x,n)
+  a=[]
+  count=0
+  while count<n
+    a.push(x)
+  count=count+1
+  end
+
+  b=a.inject(:*)
+   c=b.to_s.split('')
+  h=c.map! do |i| i.to_i
+  end
+  d=h.inject(:+)
+  return d
+end
